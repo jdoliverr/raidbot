0f154184b2d5c4f3846c0bfa12ca80efa981321c
Jon <jdog992@gmail.com>
2020-09-01 19:34:25 -0400

    help command

---
+++ commands/help.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'help',
+    description: 'sends a list of commands',
+    execute(message, args) {
+        message.channel.send(`
+        Commands-
+**!spreadsheet**
+**!ramuh**
+**!raid**
+        `);
+    }
+};
