bd1e34ba97d7e93764a88bc85e8cb51b4f3ae569
Jon <jdog992@gmail.com>
2020-09-02 22:28:10 -0400

    friend

---
+++ commands/friend.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'friend',
+    description: 'meme',
+    execute(message, args) {
+        message.channel.send('Overrated');
+    }
+};
--- commands/help.js
+++ commands/help.js
@@ -11,6 +11,7 @@ module.exports = {
 **!darkness**
 **!shivaone**
 **!shivatwo**
+**!friend**
         `);
     }
 };
