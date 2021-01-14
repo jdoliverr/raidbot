9f7696e25f885ceb386d7f9624d897e4e87b12ef
Jon <jdog992@gmail.com>
2021-01-13 20:59:35 -0500

    added notes

--- commands/notes.js
+++ commands/notes.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'notes',
     description: 'link to raid notes',
     execute(message, args) {
-        message.channel.send('');
+        message.channel.send('https://docs.google.com/drawings/d/1oOIPXh8P-rgGlgUR7M6eypjzy00BFH2W1dXYz7tItTA/edit');
     }
 };
